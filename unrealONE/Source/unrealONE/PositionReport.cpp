4a573de8d88f5ac97accdc892a94cff36938376e
Vignesh\Windows 8 <viggu.sivarajan1998@gmail.com>
2018-02-14 17:05:41 +0530

    StageONE

--- unrealONE/Source/unrealONE/PositionReport.cpp
+++ unrealONE/Source/unrealONE/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -9,7 +9,6 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 	// ...
 }
 
@@ -19,7 +18,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	FName ObjectName=GetOwner()->GetFName();
+	UE_LOG(LogTemp, Warning, TEXT("Position of %s is at %s"), *ObjectName.ToString(), *ObjectPos);
 	
 }
 
